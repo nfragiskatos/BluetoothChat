3248093b97bd3a1bfbbaa3de92333921039ef08a
Nicholas Fragiskatos <nicholas.fragiskatos@pm.me>
2023-03-15 20:33:00 -0700

    - created FoundDeviceReceiver to act as the app's broadcast receiver for capturing any intents that match the BluetoothDevice.ACTION_FOUND filter - finished implementing the rest of the AndroidBluetoothController

--- app/src/main/java/com/nfragiskatos/bluetoothchat/data/chat/AndroidBluetoothController.kt
+++ app/src/main/java/com/nfragiskatos/bluetoothchat/data/chat/AndroidBluetoothController.kt
@@ -2,8 +2,10 @@ package com.nfragiskatos.bluetoothchat.data.chat
 
 import android.Manifest
 import android.annotation.SuppressLint
+import android.bluetooth.BluetoothDevice
 import android.bluetooth.BluetoothManager
 import android.content.Context
+import android.content.IntentFilter
 import android.content.pm.PackageManager
 import com.nfragiskatos.bluetoothchat.domain.chat.BluetoothController
 import com.nfragiskatos.bluetoothchat.domain.chat.BluetoothDeviceDomain
@@ -32,20 +34,38 @@ class AndroidBluetoothController(private val context: Context) : BluetoothContro
     override val pairedDevices: StateFlow<List<BluetoothDeviceDomain>>
         get() = _pairedDevices.asStateFlow()
 
+    private val foundDeviceReceiver = FoundDeviceReceiver { device ->
+        _scannedDevices.update { devices ->
+            val newDevice = device.toBluetoothDeviceDomain()
+            if (newDevice in devices) devices else devices + newDevice
+        }
+    }
+
     init {
         updatePairedDevices()
     }
 
     override fun startDiscovery() {
-        TODO("Not yet implemented")
+        if (!hasPermission(Manifest.permission.BLUETOOTH_SCAN)) {
+            return
+        }
+        context.registerReceiver(
+            foundDeviceReceiver,
+            IntentFilter(BluetoothDevice.ACTION_FOUND)
+        )
+        updatePairedDevices()
+        bluetoothAdapter?.startDiscovery()
     }
 
     override fun stopDiscovery() {
-        TODO("Not yet implemented")
+        if (!hasPermission(Manifest.permission.BLUETOOTH_SCAN)) {
+            return
+        }
+        bluetoothAdapter?.cancelDiscovery()
     }
 
     override fun release() {
-        TODO("Not yet implemented")
+        context.unregisterReceiver(foundDeviceReceiver)
     }
 
     private fun updatePairedDevices() {
