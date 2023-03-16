88a5b9d9bc2bb8ddc09a7d8881af5d5d249ae56c
Nicholas Fragiskatos <nicholas.fragiskatos@pm.me>
2023-03-15 20:03:52 -0700

    - created initial impl of the BluetoothController

---
+++ app/src/main/java/com/nfragiskatos/bluetoothchat/data/chat/AndroidBluetoothController.kt
@@ -0,0 +1,41 @@
+package com.nfragiskatos.bluetoothchat.data.chat
+
+import android.bluetooth.BluetoothManager
+import android.content.Context
+import com.nfragiskatos.bluetoothchat.domain.chat.BluetoothController
+import com.nfragiskatos.bluetoothchat.domain.chat.BluetoothDeviceDomain
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+
+class AndroidBluetoothController(private val context: Context) : BluetoothController {
+
+    private val bluetoothManager by lazy {
+        context.getSystemService(BluetoothManager::class.java)
+    }
+
+    private val bluetoothAdapter by lazy {
+        bluetoothManager?.adapter
+    }
+
+    private val _scannedDevices = MutableStateFlow<List<BluetoothDeviceDomain>>(emptyList())
+    private val _pairedDevices = MutableStateFlow<List<BluetoothDeviceDomain>>(emptyList())
+
+    override val scannedDevices: StateFlow<List<BluetoothDeviceDomain>>
+        get() = _scannedDevices.asStateFlow()
+
+    override val pairedDevices: StateFlow<List<BluetoothDeviceDomain>>
+        get() = _pairedDevices.asStateFlow()
+
+    override fun startDiscovery() {
+        TODO("Not yet implemented")
+    }
+
+    override fun stopDiscovery() {
+        TODO("Not yet implemented")
+    }
+
+    override fun release() {
+        TODO("Not yet implemented")
+    }
+}
